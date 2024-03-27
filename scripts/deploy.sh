ca92cef1237579c7fc735e8918895eab999d942b
dbuchko <dbuchko@vmware.com>
2024-03-27 15:52:34 -0400

    Polish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,24 +5,24 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 
-if cf m | grep -q "p\.config-server"; then
-  export service_name="p.config-server"
-elif cf m | grep -q "p-config-server"; then
-  export service_name="p-config-server"
-else
-  echo "Can't find SCS Config Server in marketplace. Have you installed the SCS Tile?"
-  exit 1
-fi
+#if cf m | grep -q "p\.config-server"; then
+#  export service_name="p.config-server"
+#elif cf m | grep -q "p-config-server"; then
+#  export service_name="p-config-server"
+#else
+#  echo "Can't find SCS Config Server in marketplace. Have you installed the SCS Tile?"
+#  exit 1
+#fi
 
-echo -n "Creating Config Server..."
-{
-  cf create-service -c '{ "git": { "uri": "https://github.com/dbuchko/cook-config", "label": "main"  } }' ${service_name} standard cook-config-server
-} &>/dev/null
-until [ "$(cf service cook-config-server | grep -c "succeeded")" -eq 1 ]; do
-  echo -n "."
-done
+#echo -n "Creating Config Server..."
+#{
+#  cf create-service -c '{ "git": { "uri": "https://github.com/dbuchko/cook-config", "label": "main"  } }' ${service_name} standard cook-config-server
+#} &>/dev/null
+#until [ "$(cf service cook-config-server | grep -c "succeeded")" -eq 1 ]; do
+#  echo -n "."
+#done
 
 echo
 echo "Config Server created. Pushing application."
-cf push -p "$@"
+cf push -p "$@" -f ../manifest.yml -b java_buildpack_offline
 echo "Done!"
